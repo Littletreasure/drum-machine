2f3b1a3709d7239d1b658beb50b6f226dc96434f
Littletreasure <ruth_sargent@yahoo.co.uk>
2020-08-17 18:11:25 +0100

    add functionality, power and display

--- src/App.js
+++ src/App.js
@@ -6,22 +6,37 @@ import "./css/App.css";
 class App extends Component {
   state = {
     buttonId: "",
-    power: true,
+    power: false,
   };
 
   handleClick = (id, keyTrigger) => {
-    const sound = document.getElementById(keyTrigger);
-    sound.currentTime = 0;
-    sound.play();
-    this.setState({ buttonId: id });
+    if (this.state.power) {
+      const sound = document.getElementById(keyTrigger);
+      sound.currentTime = 0;
+      sound.play();
+      this.setState({ buttonId: id });
+    }
   };
+
+  handlePowerClick = () => {
+    this.setState((prevState) => ({
+      power: !prevState.power,
+      buttonId: "",
+    }));
+  };
+
   render() {
+    const { buttonId, power } = this.state;
     return (
       <div className="App">
         <h1 className="header">Drum Machine</h1>
         <div className="drumMachine">
           <PadContainer handleClick={this.handleClick} />
-          <Controls />
+          <Controls
+            buttonId={buttonId}
+            power={power}
+            handlePowerClick={this.handlePowerClick}
+          />
         </div>
       </div>
     );
--- src/Components/Controls.jsx
+++ src/Components/Controls.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
+
 import "../css/App.css";
 
 class Controls extends Component {
   state = {};
   render() {
-    return <p>Controls</p>;
+    return (
+      <div className="controls">
+        <div className="powerLine">
+          <p>Power</p>
+          <FontAwesomeIcon
+            icon={faPowerOff}
+            onClick={this.props.handlePowerClick}
+            className={this.props.power ? "powerOn" : "powerOff"}
+          />
+        </div>
+        <p className="display">{this.props.buttonId}</p>
+      </div>
+    );
   }
 }
 
